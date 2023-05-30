538a3296c8f90666fbd0305a2cc6fe64603d4391
Fatmatrn <ftmtrn3488@gmail.com>
2023-05-30 19:25:34 +0300

    :4

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ let arr = [335,3594,82,431,123,2097,716,541,19]
 for(let i=0; i<=arr.length;i++){
 
 getShowDetails(arr[i], (show)=>{
-    console.log(show.premiered.substring(0,4));
+    //console.log(show.premiered.substring(0,4));
     let span = document.createElement("span");
     span.innerHTML=show.premiered.substring(0,4);
 
@@ -22,3 +22,55 @@ getShowDetails(arr[i], (show)=>{
     
     });
 }
+
+let timeoutSearch = null;
+const lstTvShows = document.getElementById("lstTvShows");
+
+
+document.getElementById("txtSearch").addEventListener("input", (e) => {
+  const query = e.target.value;
+
+  if (timeoutSearch) clearTimeout(timeoutSearch);
+  timeoutSearch = setTimeout(() => {
+    searchShows(query, (shows) => {
+      createMovies(shows);
+    });
+  }, 500);
+});
+
+lstTvShows.addEventListener("click", (e)=>{
+  const selectedCard = e.target.closest(".card");
+  const showId = selectedCard.dataset.show;
+  location.href = `107-tvshow-details.html?id=${showId}`;
+});
+
+const createMovies = (shows) => {
+  lstTvShows.innerHTML = "";
+  shows.forEach((item) => {
+    const movieCard = createMovieCard(item);
+    lstTvShows.insertAdjacentHTML("afterbegin", movieCard);
+  });
+};
+
+const createMovieCard = (item) => {
+  const { id, image, name, genres } = item.show;
+
+  let movieImage = "../img/purple.png";
+
+  if (image) {
+    movieImage = image.medium;
+  }
+
+
+  return `
+    <div class="col">
+        <div class="card h-100" style="cursor:pointer" data-show="${id}">
+          <img src=${movieImage} class="card-img-top" alt="${name}" />
+          <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${genres.join("-")}</p>
+          </div>
+        </div>
+    </div>`;
+};
+
